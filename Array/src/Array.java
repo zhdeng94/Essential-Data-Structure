50b259f92d1047300f021c0b665d591bd83e2890
Zihao Deng <zhdeng@Karpathos.local>
2020-04-27 18:24:21 -0400

    add array project

---
+++ Array/src/Array.java
@@ -0,0 +1,12 @@
+/**
+ * This is my own implementation of an array data structure that supports
+ * generic data type and it can dynamically maintain the size of the array.
+ *
+ * @author Zihao Deng
+ * @param <E> generic data type
+ * @since 2020-04-27
+ */
+
+public class Array<E> {
+
+}
