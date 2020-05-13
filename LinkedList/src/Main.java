76ad3ab33d21123e112d0e89d9d2839ae42086c2
Zihao Deng <zhdeng@Karpathos.local>
2020-05-13 14:18:49 -0400

    Add circular linked list implementation and test it in main

--- LinkedList/src/Main.java
+++ LinkedList/src/Main.java
@@ -1,7 +1,10 @@
+import java.util.Currency;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        // test singly linked list
         SinglyLinkedList<Integer> list1 = new SinglyLinkedList<>();
 
         for (int i = 0; i < 10; i++) {
@@ -13,5 +16,22 @@ public class Main {
         }
 
         System.out.println(list1.remove(2));
+
+        System.out.println();
+
+        // test circular linked list
+        CircularLinkedList<Integer> list2 = new CircularLinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list2.addLast(i);
+            if (i % 3 == 0) {
+                list2.removeFirst();
+            }
+            System.out.println(list2);
+        }
+
+        list2.add(100, 4);
+        list2.remove(6);
+        System.out.println(list2);
     }
 }
